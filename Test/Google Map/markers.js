937795065b28f26fc24ead4f5a9cedb4cac890a8
quentin-chaumette <quentinchaumette@hotmail.fr>
2014-10-07 22:38:16 +0200

    Geoloc opérationnelle + ajout de points (markers)
    Page index : géolocalise l’utilisateur : voir dans ‘geoloc.js’
    Page marker.html : point ajouté : voir dans ‘marker.js’

---
+++ Test/Google Map/markers.js
@@ -0,0 +1,47 @@
+	window.onload = function() {
+		// Défilé pas loin
+		var myLatlng = new google.maps.LatLng(48.85273555769621,2.4280994430596543);
+		
+		// Carte centrée sur le marker
+		var myMapOptions = {
+			zoom: 16,
+			center: myLatlng,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};
+		
+		// Création de la carte
+		var myMap = new google.maps.Map(
+			document.getElementById('map'),
+			myMapOptions
+			);
+		
+/*		// Création de l'icône
+		var myMarkerImage = new google.maps.MarkerImage('img/icone_marker.png');*/
+		
+		// Création du marker
+		var myMarker = new google.maps.Marker({
+			// Coordonnées du marker
+			position: myLatlng, 
+			map: myMap,
+			// Nous ajoutons un paramètre supplémentaire
+			// icon pour lequel nous donnons le MarkerImage
+			// que nous venons de créer.
+	/*		icon: myMarkerImage,		*/
+			title: "Cinéma Pathé Bellecour"
+		});
+		
+		// Options de la fenêtre
+		var myWindowOptions = {
+			content:
+			'<h6>Défilé Exemple</h6>'+
+			'<p><a href="http://google.fr" title="Lien">Voir la vidéo</a></p>'
+		};
+		
+		// Création de la fenêtre
+		var myInfoWindow = new google.maps.InfoWindow(myWindowOptions);
+		
+		// Affichage de la fenêtre au click sur le marker
+		google.maps.event.addListener(myMarker, 'click', function() {
+			myInfoWindow.open(myMap,myMarker);
+		});
+	}
