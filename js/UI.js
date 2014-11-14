f28103cc2076b0bc3394806386c42c9f603cf40e
Joanna DENIS <joanna.denis8@gmail.com>
2014-11-14 14:10:58 +0100

    Inté homepage
    Intégration du design de la homepage +rangement des dossiers

---
+++ js/UI.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/*var UI = {
+
+	affiche_liste_events : function(data, callback){
+		for (var i=0;i<data.events.length;i++){
+			
+			
+			//   APPELER LES DONNEES RECUPEREES DU FICHIER JSON : 
+			//   data.events[i].champ_du_fichier_JSON
+			
+			console.log(data.events[i].titre);
+			console.log(data.events[i].sous_titre);
+			console.log(data.events[i].image);
+			console.log(data.events[i].timestamp);
+			console.log(data.events[i].lat);
+			console.log(data.events[i].lng);
+			console.log('–––––––––––––––––––––––––––––––––');			
+			
+			
+	//			=> utilisable également depuis control.js en callback de model.getJSON
+		}
+		callback.call(this);									// signifie à control.js que c'est fini
+	}
+}*/
---
+++ js/control.js
@@ -0,0 +1,20 @@
+"use strict";
+
+// var control={};
+
+
+// model.getJSON('js/events.json', function(data) {	 
+// 	// si model.getJSON() a récupéré les events :
+// 	UI.affiche_liste_events(data,function(){
+// 		console.log('callback ok : évènements récupérés'); 	// effectué si callback de la fonction : UI.affiche_liste_events()
+// 	});
+// }, 
+// function(status) {
+// 	// si model.getJSON récup pas events :
+//   	alert("Désolé, évènements indisponibles pour le moment.");					
+// });
+
+
+
+
+
