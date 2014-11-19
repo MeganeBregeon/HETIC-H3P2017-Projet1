8e723f8eafb77b12e2ff19f6da816de551b2a19a
Joanna DENIS <joanna.denis8@gmail.com>
2014-11-19 11:51:45 +0100

    nouvelle inté

--- js/UI.js
+++ js/UI.js
@@ -13,12 +13,20 @@ var UI = {
 			div_event.classList.add('event');									// assigne la classe event à cette div
 			div_a_remplir.appendChild(div_event);								// place la div dans la fenêtre
 
-			// Image :
-			var image=document.createElement('img');							// crée l'élément html img
-			image.setAttribute('src',data.events[i].image);						// assigne l'url de l'image
-			image.setAttribute('alt',"image de l'évènement");					// assigne le texte alternatif
-			image.setAttribute('class','image_event');							// assigne la classe image_event
+			// // Image :
+			// var image=document.createElement('img');							// crée l'élément html img
+			// image.setAttribute('src',data.events[i].image);						// assigne l'url de l'image
+			// image.setAttribute('alt',"image de l'évènement");					// assigne le texte alternatif
+			// image.setAttribute('class','image_event');							// assigne la classe image_event
+			// div_event.appendChild(image);										// place l'image dans la div_event
+
+			// Video :
+			var image=document.createElement('video');							// crée l'élément html img
+			image.setAttribute('src',data.events[i].video);						// assigne l'url de l'image
+			image.setAttribute('class','video_event');							// assigne la classe image_event
 			div_event.appendChild(image);										// place l'image dans la div_event
+
+
 
 			// Titre :
 			var texte_titre=document.createTextNode(data.events[i].titre);		// récup. le texte du titre
@@ -35,13 +43,22 @@ var UI = {
 			div_event.appendChild(lieu);										// place le sous titre dans la div_event
 
 
-			// Temps restant (A FAIRE) :
-			var temps_restant = '3 jours';										// FAUT RECUPERER ET CONVERTIR TIMESTAMP
 
-			// Préparation des div avec la distance :
-			var distance=document.createElement('span');						// crée l'élément html h6 pour le texte
-			distance.setAttribute('class','distance_event');					// assigne la classe
-			div_event.appendChild(distance);									// place la distance dans la div_event
+			// Temps restant (A FAIRE) :
+			var temps_restant = document.createTextNode(data.events[i].timestamp);
+			var timestamp = document.createElement('h3');
+			timestamp.appendChild(temps_restant);
+			timestamp.setAttribute('class','compte-rebours');
+			div_event.appendChild(timestamp);
+
+
+
+													// FAUT RECUPERER ET CONVERTIR TIMESTAMP
+
+			// // Préparation des div avec la distance :
+			// var distance=document.createElement('span');						// crée l'élément html h6 pour le texte
+			// distance.setAttribute('class','distance_event');					// assigne la classe
+			// div_event.appendChild(distance);									// place la distance dans la div_event
 
 
 
@@ -51,14 +68,14 @@ var UI = {
 		callback_final.call(this);
 	},
 
-	ajoute_distance_events : function(tableau_distances, callback){
-		var zone_distance = document.querySelectorAll('.distance_event');
-		for (var i=0;i<tableau_distances.length;i++){
-			var texte_distance = document.createTextNode(tableau_distances[i]);
-			zone_distance[i].appendChild(texte_distance);						// place le texte dans le span
-		}
-		callback.call(this);
-	}
+	// ajoute_distance_events : function(tableau_distances, callback){
+	// 	var zone_distance = document.querySelectorAll('.distance_event');
+	// 	for (var i=0;i<tableau_distances.length;i++){
+	// 		var texte_distance = document.createTextNode(tableau_distances[i]);
+	// 		zone_distance[i].appendChild(texte_distance);						// place le texte dans le span
+	// 	}
+	// 	callback.call(this);
+	// }
 
 
 }// fin UI
